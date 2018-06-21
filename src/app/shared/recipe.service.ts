4cc932f721edcb87c8467608e332cdf925876cf7
doctral <lifei.8886196@gmail.com>
2018-06-21 01:43:56 -0400

    add models and services

---
+++ src/app/shared/recipe.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {Recipe} from "../models/recipe.model";
+import {Subject} from "rxjs/";
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+
+ // recipesChanged = new Subject<Recipe[]>();
+   recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG'),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg')
+  ];
+
+  constructor() { }
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
+}
