1f5ab6347e891f477454862a0240f61b274cf68f
doctral <lifei.8886196@gmail.com>
2018-06-28 00:42:17 -0400

    get a problem to set values of ngForm

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,19 +1,64 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import {Ingredient} from '../../models/ingredient.model';
 import {IngredientService} from '../../shared/ingredient.service';
 import {NgForm} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
   @ViewChild('f') slForm: NgForm;
   editMode = false;
-  constructor() { }
+  sub: Subscription;
+  editItemIndex:number;
+  editItem:Ingredient;
+
+  constructor(private ingredientService: IngredientService) { }
 
   ngOnInit() {
+    this.sub=this.ingredientService.startedEditing.subscribe(
+      (index:number) => {
+        this.editItemIndex=index;
+        this.editMode=true;
+        debugger;
+        this.editItem=this.ingredientService.getIngredient(index);
+        // has problem to set form values
+        this.slForm.setValue({
+          name: this.editItem.name,
+          amount: this.editItem.amount
+        })
+      }
+    );
+  }
+
+  onSubmit(form: NgForm){
+    const value=form.value;
+    const newIngredient=new Ingredient(value.name, value.amount);
+    if(this.editMode){
+      this.ingredientService.updateIngredient(this.editItemIndex, newIngredient);
+    }
+    else{
+      this.ingredientService.addIngredient(newIngredient);
+    }
+    this.editMode=false;
+    form.onReset();
+  }
+
+  onClear(){
+    this.slForm.onReset();
+    this.editMode=false;
+  }
+
+  onDelete(){
+    this.ingredientService.deleteIngredient(this.editItemIndex);
+    this.onClear();
+  }
+
+  ngOnDestroy(){
+    this.sub.unsubscribe();
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Ingredient} from '../models/ingredient.model';
 import {IngredientService} from '../shared/ingredient.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit,OnDestroy {
   ingredients:Ingredient[];
-
+  private sub: Subscription;
   constructor(private ingredientService:IngredientService) { }
 
   ngOnInit() {
     this.ingredients=this.ingredientService.getIngredients();
+    this.sub=this.ingredientService.ingredientsChanged.subscribe((ingre: Ingredient[]) => {
+      this.ingredients=ingre
+      }
+    );
+  }
+
+  onEditItem(index:number){
+    this.ingredientService.startedEditing.next(index);
+  }
+
+  ngOnDestroy(){
+    this.sub.unsubscribe();
   }
 
 }
